1ac0641cb9a32df839b07511cc7e6a8138424df0
slavaaaaa88 <d4896869@gmail.com>
2025-07-30 00:53:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');   // accessibility для модальных окон
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+
 
 
